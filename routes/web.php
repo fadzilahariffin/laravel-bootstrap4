b2e3812041c84e58f1bb613b2539602b1e3188f5
Fadzilah Ariffin <fadzilah9595@gmail.com>
2021-03-27 12:36:52 +0800

    add all files

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\LandingPageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[LandingPageController::class,'index'])->name('landing_page');
+
+
+Auth::routes();
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+});
+
+
